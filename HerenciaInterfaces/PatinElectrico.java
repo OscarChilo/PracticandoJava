6f01400771031167bdbebe0545fce1e8690f8a36
OscarChilo <ochilo@unsa.edu.pe>
2023-07-05 00:05:58 -0500

    update

---
+++ HerenciaInterfaces/PatinElectrico.java
@@ -0,0 +1,17 @@
+package HerenciaInterfaces;
+
+public class PatinElectrico implements VehiculoRecargable {
+
+    @Override
+    public void recargar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'recargar'");
+    }
+
+    @Override
+    public void acelerar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'acelerar'");
+    }
+
+}
