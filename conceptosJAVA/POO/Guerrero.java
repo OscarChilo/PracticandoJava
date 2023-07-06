458148d0d6dc9b3d592a9286769bcbdc7256ce24
OscarChilo <ochilo@unsa.edu.pe>
2023-07-06 13:11:09 -0500

    conceptos sobre uso de metodos

--- conceptosJAVA/POO/Guerrero.java
+++ conceptosJAVA/POO/Guerrero.java
@@ -5,8 +5,33 @@ package conceptosJAVA.POO;
  */
 public class Guerrero {
 
-    String nombre;
-    int fuerza;
-    int salud;
+    private String nombre;
+    private int fuerza, salud;
+
+    // get devolver
+    public String getNombre() {
+        return nombre;
+    }
+
+    // set asignamos(VA RECIBIR)
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public int getSaludg() {
+        return salud;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
 
 }
--- conceptosJAVA/POO/index.java
+++ conceptosJAVA/POO/index.java
@@ -2,17 +2,14 @@ package conceptosJAVA.POO;
 
 public class index {
     public static void main(String[] args) {
-        Guerrero g1 = new Guerrero();
-        Guerrero g2 = new Guerrero();
+        Guerrero g1 = new Guerrero(); // estamos haciendo una instacia la clase Guerrero
+        // Guerrero g2 = new Guerrero();
 
-        g1.nombre = "Goku";
-        g1.fuerza = 56;
-        g1.salud = 99;
+        g1.setNombre("Goku");
+        g1.setFuerza(45);
+        g1.setSalud(90);
 
-        g2.nombre = "vegueta";
-        g2.fuerza = 87;
-        g2.salud = 85;
-
-        System.out.println("el nombre de mi guerrero es : " + g1.nombre);
+        System.out.println("el nombre de mi guerrero es:" + g1.getNombre() + " la fuerza que tiene  es "
+                + g1.getFuerza() + " y cantida de vida que tiene es " + g1.getSaludg());
     }
 }
